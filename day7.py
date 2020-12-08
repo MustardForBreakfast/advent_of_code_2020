13be5b037d15f532f06fe2fbb657f0bb811194bf
Justin Wayne <justin@bestow.co>
2020-12-07 21:22:26 -0600

    add some linting and typing configs

--- day7.py
+++ day7.py
@@ -5,25 +5,27 @@ from functools import reduce
 from pathlib import Path
 
 
-def _parse_container_contents(rule_line):
+def _parse_container_contents(rule_line: str) -> t.Tuple[str, str]:
     """
     'light red bags contain 1 bright white bag, 2 muted yellow bags.' ->
     ('light red', '1 bright white bag, 2 muted yellow bags')
     """
     groups = re.search(r"^([a-z\s]+) bags contain (.+).$", rule_line)
+    assert groups is not None
     container = groups[1]
     contents = groups[2]
 
     return (container, contents)
 
 
-def _parse_content_bag_and_count(content_str):
+def _parse_content_bag_and_count(content_str: str) -> t.Tuple[str, int]:
     """5 faded blue bags -> ('faded blue', 5)"""
     groups = re.search(r"^(\d) ([a-z\s]+) bags*$", content_str)
+    assert groups is not None
     return (groups[2], int(groups[1]))
 
 
-def _parse_contents(contents_str):
+def _parse_contents(contents_str: str) -> t.Tuple[t.Tuple[str, int], ...]:
     """
     '5 faded blue bags, 6 dotted black bags' ->
     (('faded blue', 5), ('dotted black', 6))
@@ -36,7 +38,7 @@ def _parse_contents(contents_str):
     return tuple(map(lambda cb: _parse_content_bag_and_count(cb), splitstrip))
 
 
-def parse_rule(rule_line: str) -> t.Tuple[str, t.Tuple[int, str]]:
+def parse_rule(rule_line: str) -> t.Tuple[str, t.Tuple[t.Tuple[str, int], ...]]:
     """
     'light red bags contain 1 bright white bag, 2 muted yellow bags.' ->
     ('light red', ((1, 'bright white'), (2, 'muted yellow')))
@@ -71,12 +73,15 @@ def parse_rules(data: str) -> t.Dict[str, t.Dict[str, int]]:
     return reduce(lambda acc, r: {**acc, r[0]: dict(r[1])}, parsed, {})
 
 
-def get_content_bag_count(target_bag: str, rules: t.Dict[str, t.Dict[str, int]]) -> int:
+def get_content_bag_count(
+    target_bag: str, rules: t.Dict[str, t.Dict[str, int]]
+) -> int:
     bag = rules[target_bag]
     sum_top_contents = reduce(lambda acc, count: acc + count, bag.values(), 0)
     # return (sum_top_contents)
     sum_contents_contents = reduce(
-        lambda acc, b_name: acc + (get_content_bag_count(b_name, rules) * bag[b_name]),
+        lambda acc, b_name: acc
+        + (get_content_bag_count(b_name, rules) * bag[b_name]),
         bag.keys(),
         0,
     )
@@ -85,7 +90,9 @@ def get_content_bag_count(target_bag: str, rules: t.Dict[str, t.Dict[str, int]])
 
 # ***** Used in Part 1: *****
 def can_contain(
-    target_bag: str, possible_content_bag: str, rules: t.Dict[str, t.Dict[str, int]]
+    target_bag: str,
+    possible_content_bag: str,
+    rules: t.Dict[str, t.Dict[str, int]],
 ) -> bool:
     target = rules[target_bag]
     contents = target.keys()
@@ -93,7 +100,9 @@ def can_contain(
     if possible_content_bag in contents:
         return True
 
-    return any(map(lambda b: can_contain(b, possible_content_bag, rules), contents))
+    return any(
+        map(lambda b: can_contain(b, possible_content_bag, rules), contents)
+    )
 
 
 def get_bag_count(
@@ -104,7 +113,8 @@ def get_bag_count(
             filter(
                 lambda b: b is True,
                 map(
-                    lambda b: can_contain(b, possible_content_bag, rules), rules.keys()
+                    lambda b: can_contain(b, possible_content_bag, rules),
+                    rules.keys(),
                 ),
             )
         )
